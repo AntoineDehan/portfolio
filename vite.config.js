c041b733a6a360f92ce846bb00ebbdca494448b2
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-02-08 13:35:02 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.png", "**/*.PNG"],
-  base: "portfolio",
+  base: "/portfolio/",
 });
