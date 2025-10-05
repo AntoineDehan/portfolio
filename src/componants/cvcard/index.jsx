fa81c31eceed4c833bb7f7e658a01423fa8603c9
AntoineDehan <antoine.dehandartial@gmail.com>
2025-10-05 14:42:45 +0200

    feat(card): ajout d'une description

--- src/componants/cvcard/index.jsx
+++ src/componants/cvcard/index.jsx
@@ -1,18 +1,19 @@
 import Tag from "../tag";
 
-import "../../styles/css/cvcard/style.css";
+import "../../styles/scss/cvcard/style.scss";
 
 function CvCard({ data }) {
   return (
     <div className="cv-card-conteneur">
       {data?.map((card) => {
-        const { id, image, title, date, content, tags } = card;
+        const { id, image, title, description, date, content, tags } = card;
         return (
           <div className="cv-card" key={id}>
             <img src={image} alt="" />
             <div className="cv-card-contenu">
               <h3 className="cv-card-title">{title}</h3>
               <p className="cv-card-date">{date}</p>
+              <p className="cv-card-description">{description}</p>
               <p className="cv-card-content">{content}</p>
               <Tag data={tags} />
             </div>
